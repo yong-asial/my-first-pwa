7ffd52c5f72e1d4a9e7721313074b56a128c07e8
yong <yong@asial.co.jp>
2019-01-16 14:23:15 +0900

    initial

---
+++ www/service-worker.js
@@ -0,0 +1,58 @@
+var CACHE_NAME = 'my-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      if (response) {
+        return response;
+      }
+
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(
+        function (response) {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        }
+      );
+    })
+  );
+});
